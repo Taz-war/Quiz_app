8f06aa0842a4addf158c9b12303cfb76fa11b521
Taz-war <fahimtazwer@gmail.com>
2023-09-14 02:32:50 +0600

    truefalse component added and multiple component call

--- src/Components/MultipleChoice.jsx
+++ src/Components/MultipleChoice.jsx
@@ -16,15 +16,17 @@ import {
   Typography,
 } from "@mui/material";
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import AddIcon from '@mui/icons-material/Add';
 
 const MultipleChoice = () => {
   const [isPending,startTransition]=useTransition()
   const [multipleChoice, setMultipleChoice] = useState({ 1: "", 2: "" });
   const [question, setQuestion] = useState("");
-  const [options, setOptions] = useState({});
+  const [point, setPoint] = useState(0);
   let tempQuestion = {
     "Question":question,
-    "Options":multipleChoice
+    "Options":multipleChoice,
+    "Point": point
   }
   let index = 0;
   ///add options////
@@ -55,7 +57,7 @@ const deleteOption = (optionName) => {
   console.log(tempQuestion)
   return (
     <>
-      <Card sx={{ minWidth: 275, bgcolor: "#F5F7F8",mt:2,p:2,textAlign:'left' }}>
+      <Card sx={{ minWidth: 275, bgcolor: "#F5F7F8",mt:2,p:2,textAlign:'left',mb:2 }}>
         <Grid container columns={12} columnSpacing={2}>
         <Grid item xs={8}>
         <TextField variant="outlined" label="Question" fullWidth sx={{ mb: 2 ,mt:2}} onBlur={(e) => setQuestion(e.target.value)}>
@@ -63,7 +65,7 @@ const deleteOption = (optionName) => {
         </TextField>
         </Grid>
         <Grid item xs={4}>
-          <TextField variant="outlined" label="Point" fullWidth sx={{ mb: 2 ,mt:2}}>Point</TextField>
+          <TextField variant="outlined" label="Point" fullWidth sx={{ mb: 2 ,mt:2}} onChange={(e)=>setPoint(e.target.value)}>Point</TextField>
         </Grid>
         </Grid>
         {Object.keys(multipleChoice).map((option, i) => {
@@ -87,6 +89,7 @@ const deleteOption = (optionName) => {
           size="small"
           variant="outlined"
           sx={{mt:2,bgcolor:'#E9EEFB',color:'navy'}}
+          startIcon={<AddIcon />}
           onClick={addOption}
         >
           ADD MORE Options
--- src/Pages/Create_Quiz.jsx
+++ src/Pages/Create_Quiz.jsx
@@ -1,18 +1,44 @@
 import React, { useState } from 'react';
-import { Box, Button, Card, CardActions, CardContent, Container, Paper, TextField } from '@mui/material';
+import { Box, Button, Paper, Container } from '@mui/material';
 import MultipleChoice from '../Components/MultipleChoice';
+import ShortQuestion from '../Components/ShortQuestion';
+import TrueFalse from '../Components/TrueFalse';
 
 const Create_Quiz = () => {
-  const [openMcq,setOpenMcq] = useState(false);
-  const [openShortAnswer,setOpenShortAnswer] = useState(false);
-  const [openTrueFalse,setOpenTrueFalse] = useState(false);
+  const [componentsToRender, setComponentsToRender] = useState([]);
+
+  // Function to add components to the list
+  const addComponent = (component) => {
+    setComponentsToRender([...componentsToRender, component]);
+  };
+
   return (
     <Paper component={Container}>
       <Box p={2}>
-        {openMcq && <MultipleChoice />}
-      <Button variant='contained' sx={{mr:2}} onClick={()=>setOpenMcq(true)}> Multiple Choice</Button>
-      <Button variant='contained'sx={{mr:2}}> Short Answer</Button>
-      <Button variant='contained'sx={{mr:2}}> True/False</Button>
+        {componentsToRender.map((component, index) => (
+          <div key={index}>{component}</div>
+        ))}
+        <Button
+          variant="contained"
+          sx={{ mr: 2 }}
+          onClick={() => addComponent(<MultipleChoice key={componentsToRender.length} />)}
+        >
+          Multiple Choice
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ mr: 2 }}
+          onClick={() => addComponent(<ShortQuestion key={componentsToRender.length} />)}
+        >
+          Short Answer
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ mr: 2 }}
+          onClick={() => addComponent(<TrueFalse key={componentsToRender.length} />)}
+        >
+          True/False
+        </Button>
       </Box>
     </Paper>
   );
