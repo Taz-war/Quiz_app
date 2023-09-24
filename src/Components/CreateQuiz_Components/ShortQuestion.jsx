a7a7c212be72197a5bf8d7c28b94ba872647b844
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2023-09-24 12:07:07 +0600

    a

--- src/Components/CreateQuiz_Components/ShortQuestion.jsx
+++ src/Components/CreateQuiz_Components/ShortQuestion.jsx
@@ -2,25 +2,27 @@ import React, { useEffect, useState, useTransition } from "react";
 import {
   Button,
   Card,
+  Collapse,
   Grid,
   ListItem,
   ListItemButton,
   TextField,
+  Typography,
 } from "@mui/material";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import AddIcon from "@mui/icons-material/Add";
 
 const ShortQuestion = () => {
-  const [shortAnswer, setShortAnswer] = useState([""]);
+  const [open, setOpen] = useState(false);
+  const [shortAnswer, setShortAnswer] = useState(null);
   const [question, setQuestion] = useState("");
   const [point, setPoint] = useState(0);
 
-
   let tempShortQuestion = {
-    "Question":question,
-    "Point": point,
-    "Answer":shortAnswer
-  }
+    Question: question,
+    Point: point,
+    Answer: shortAnswer,
+  };
   console.log(tempShortQuestion);
 
   ///add options////
@@ -36,7 +38,7 @@ const ShortQuestion = () => {
     setShortAnswer([...tempArr]);
   };
 
-  const handleOptionChange =debounce( (newValue, index) => {
+  const handleOptionChange = debounce((newValue, index) => {
     const tempArr = [...shortAnswer];
     tempArr[index] = newValue;
     setShortAnswer(tempArr);
@@ -54,69 +56,98 @@ const ShortQuestion = () => {
 
   return (
     <>
-      <Card
-        sx={{
-          minWidth: 275,
-          bgcolor: "#F5F7F8",
-          mt: 2,
-          p: 2,
-          textAlign: "left",
-          mb: 2,
-        }}
-      >
-        <Grid container columns={12} columnSpacing={2}>
-          <Grid item xs={8}>
-            <TextField
-              variant="outlined"
-              label="Question"
-              fullWidth
-              sx={{ mb: 2, mt: 2 }}
-              onBlur={(e) => setQuestion(e.target.value)}
-            >
-              Question
-            </TextField>
-          </Grid>
-          <Grid item xs={4}>
-            <TextField
-              variant="outlined"
-              label="Point"
-              fullWidth
-              sx={{ mb: 2, mt: 2 }}
-              onChange={(e) => setPoint(e.target.value)}
-            >
-              Point
-            </TextField>
-          </Grid>
-        </Grid>
-        {shortAnswer.map((option, i) => {
-          return (
-            <ListItem key={i} disablePadding>
-              <ListItemButton role={undefined} dense>
-                <TextField
-                  fullWidth
-                  label={"Correct Answer (optional)"}
-                  // value={option}
-                  // placeholder={`${shortAnswer[i]}`}
-                  onChange={(e) => handleOptionChange(e.target.value,i) }
-                />
-                <DeleteOutlineOutlinedIcon
-                  sx={{ color: "red", ml: 1, fontSize: "xx-large" }}
-                  onClick={() => deleteOption(i)}
-                />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-        <Button
-          size="small"
-          variant="outlined"
-          sx={{ mt: 2, bgcolor: "#E9EEFB", color: "navy" }}
-          startIcon={<AddIcon />}
-          onClick={addOption}
+      <Collapse in={open === false}>
+        <Card
+          sx={{
+            minWidth: 275,
+            bgcolor: "#F5F7F8",
+            mt: 2,
+            p: 2,
+            textAlign: "left",
+            mb: 2,
+          }}
         >
-          ADD MORE Options
-        </Button>
-      </Card>
+          <Grid container columns={12} columnSpacing={2}>
+            <Grid item xs={8}>
+              <TextField
+                variant="outlined"
+                label="Question"
+                fullWidth
+                sx={{ mb: 2, mt: 2 }}
+                onBlur={(e) => setQuestion(e.target.value)}
+              >
+                Question
+              </TextField>
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                variant="outlined"
+                label="Point"
+                fullWidth
+                sx={{ mb: 2, mt: 2 }}
+                onChange={(e) => setPoint(e.target.value)}
+              >
+                Point
+              </TextField>
+            </Grid>
+          </Grid>
+          {shortAnswer?.map((option, i) => {
+            return (
+              <ListItem key={i} disablePadding>
+                <ListItemButton role={undefined} dense>
+                  <TextField
+                    fullWidth
+                    label={"Correct Answer (optional)"}
+                    // value={option}
+                    // placeholder={`${shortAnswer[i]}`}
+                    onChange={(e) => handleOptionChange(e.target.value, i)}
+                  />
+                  <DeleteOutlineOutlinedIcon
+                    sx={{ color: "red", ml: 1, fontSize: "xx-large" }}
+                    onClick={() => deleteOption(i)}
+                  />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+          <Button
+            size="small"
+            variant="outlined"
+            sx={{ mt: 2, bgcolor: "#E9EEFB", color: "navy" }}
+            startIcon={<AddIcon />}
+            onClick={addOption}
+          >
+            ADD MORE Options
+          </Button>
+          <Button variant="contained" onClick={() => setOpen(true)}>
+            Submit
+          </Button>
+        </Card>
+      </Collapse>
+      <Collapse in={open}>
+        <Card
+          sx={{
+            minWidth: 275,
+            bgcolor: "#F5F7F8",
+            mt: 2,
+            p: 2,
+            textAlign: "left",
+            mb: 2,
+          }}
+        >
+          <Typography fontSize={"x-large"} fontWeight={"bolder"} mb={2}>
+            {tempShortQuestion.Question}
+          </Typography>
+        
+          {shortAnswer?.map((data,i) => 
+           
+         
+              <p>{data}</p>
+           
+            
+          )}
+        </Card>
+      </Collapse>
     </>
   );
 };
--- src/Components/CreateQuiz_Components/TrueFalse.jsx
+++ src/Components/CreateQuiz_Components/TrueFalse.jsx
@@ -1,34 +1,109 @@
-import { Button, Card, Grid, TextField } from '@mui/material'
-import React, { useState } from 'react'
+import {
+  Button,
+  Card,
+  Collapse,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 
 const TrueFalse = () => {
+  const [open, setOpen] = useState(false);
   const [question, setQuestion] = useState("");
   const [point, setPoint] = useState(0);
-  const [value,setValue] = useState(null)
+  const [value, setValue] = useState(null);
   let tempTrueFalseQuestion = {
-    "Question":question,
-    "Point": point,
-    "Answer":value
-  }
-  console.log(tempTrueFalseQuestion)
+    Question: question,
+    Point: point,
+    Answer: value,
+  };
+  console.log(tempTrueFalseQuestion);
   return (
-      <Card sx={{ minWidth: 275, bgcolor: "#F5F7F8", mt: 2, p: 2, textAlign: 'left', mb: 2 }}>
+    <>
+      <Collapse in={open === false}>
+        <Card
+          sx={{
+            minWidth: 275,
+            bgcolor: "#F5F7F8",
+            mt: 2,
+            p: 2,
+            textAlign: "left",
+            mb: 2,
+          }}
+        >
           <Grid container columns={12} columnSpacing={2}>
-              <Grid item xs={8}>
-                  <TextField variant="outlined" label="Question" fullWidth sx={{ mb: 2, mt: 2 }} onBlur={(e) => setQuestion(e.target.value)}>
-                      Question
-                  </TextField>
-              </Grid>
-              <Grid item xs={4}>
-                  <TextField variant="outlined" label="Point" fullWidth sx={{ mb: 2, mt: 2 }} onChange={(e) => setPoint(e.target.value)}>Point</TextField>
-              </Grid>
-              <Grid item xs={5}>
-                  <Button variant={`${value===true?'contained':'outlined'}`}  color={`${value===true?'success':'secondary'}`} sx={{mr:2}} onClick={()=>setValue(true)}>True</Button>
-                  <Button variant={`${value===false?'contained':'outlined'}`} color={`${value===true?'secondary':'error'}`} onClick={()=>setValue(false)}>False</Button>
-              </Grid>
+            <Grid item xs={8}>
+              <TextField
+                variant="outlined"
+                label="Question"
+                fullWidth
+                sx={{ mb: 2, mt: 2 }}
+                onBlur={(e) => setQuestion(e.target.value)}
+              >
+                Question
+              </TextField>
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                variant="outlined"
+                label="Point"
+                fullWidth
+                sx={{ mb: 2, mt: 2 }}
+                onChange={(e) => setPoint(e.target.value)}
+              >
+                Point
+              </TextField>
+            </Grid>
+            <Grid item xs={5}>
+              <Button
+                variant={`${value === true ? "contained" : "outlined"}`}
+                color={`${value === true ? "success" : "secondary"}`}
+                sx={{ mr: 2 }}
+                onClick={() => setValue(true)}
+              >
+                True
+              </Button>
+              <Button
+                variant={`${value === false ? "contained" : "outlined"}`}
+                color={`${value === true ? "secondary" : "error"}`}
+                onClick={() => setValue(false)}
+              >
+                False
+              </Button>
+            </Grid>
           </Grid>
-      </Card>
-  )
-}
+          <Button variant="contained" onClick={() => setOpen(true)}>
+            Submit
+          </Button>
+        </Card>
+      </Collapse>
+      <Collapse in={open}>
+        <Card
+          sx={{
+            minWidth: 275,
+            bgcolor: "#F5F7F8",
+            mt: 2,
+            p: 2,
+            textAlign: "left",
+            mb: 2,
+          }}
+        >
+          <Typography fontSize={"x-large"} fontWeight={"bolder"} mb={2}>
+            {tempTrueFalseQuestion.Question}
+          </Typography>
+          <Button
+            variant="contained"
+            color={`${
+              tempTrueFalseQuestion.Answer === true ? "success" : "error"
+            }`}
+          >
+            {tempTrueFalseQuestion.Answer === true ? "True" : "False"}
+          </Button>
+        </Card>
+      </Collapse>
+    </>
+  );
+};
 
-export default TrueFalse
+export default TrueFalse;
