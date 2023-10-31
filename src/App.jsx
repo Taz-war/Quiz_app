5eec1ffce3d4a364ca413e0123e94eb7e4bc677f
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2023-10-31 13:52:46 +0600

    quizlist componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react'
 import CreateQuiz from './Pages/CreateQuiz';
+import QuizList from './Pages/QuizList';
 
 function App() {
   return (
     <div className="App">
-      <CreateQuiz />
+      {/* <CreateQuiz /> */}
+      <QuizList />
     </div>
   );
 }
---
+++ src/Pages/QuizList.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+import { Card, CardContent, IconButton, Typography, Box, Container } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
+
+const QuizList = () => {
+    const [quizzes,setQuizzes] = useState([])
+    const [errorMessage,setErrorMessage] = useState('')
+//   const quizzes = [
+//     { id: 1, name: "Untitled Quiz", modifiedDate: "10/30/2023" },
+//     { id: 1, name: "Untitled Quiz", modifiedDate: "10/30/2023" },
+//     { id: 1, name: "Untitled Quiz", modifiedDate: "10/30/2023" },
+//   ];
+
+  const getAllQuizes=async()=>{
+    try {
+        const response = await fetch(`http://localhost:8080/Questions`)
+        const data = await response.json()
+        setQuizzes(data)
+        
+    } catch (error) {
+        setErrorMessage(error.message)
+    }
+  }
+ 
+  useEffect(()=>{
+    getAllQuizes()
+  },[])
+
+  return (
+    <Container sx={{ p: 3 }}>
+      <Box sx={{ display: "flex", alignItems: "center", mb: 3 }}>
+        <Typography variant="h6" sx={{ flexGrow: 1 ,textAlign:'left'}}>
+          Quizzes
+        </Typography>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <IconButton>
+          <AddCircleOutlineIcon />
+        </IconButton>
+      </Box>
+
+      {quizzes.map((quiz) => (
+       
+        <Card sx={{ mb: 2,textAlign:'left' }} key={quiz.id} >
+          <CardContent>
+            <Typography variant="body1">{quiz.questionSetTitle}</Typography>
+            {/* <Typography variant="body2" color="text.secondary">
+              Modified: {quiz.modifiedDate}
+            </Typography> */}
+          </CardContent>
+        </Card>
+      ))}
+    </Container>
+  );
+};
+
+export default QuizList;
