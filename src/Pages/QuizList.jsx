ce65edd0e8ed2d27fcc3ed0168017141ab275d92
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2023-11-02 14:02:59 +0600

    hover system added

--- src/Pages/QuizList.jsx
+++ src/Pages/QuizList.jsx
@@ -1,38 +1,46 @@
 import React, { useEffect } from "react";
-import { Card, CardContent, IconButton, Typography, Box, Container } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  IconButton,
+  Typography,
+  Box,
+  Container,
+} from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import SearchIcon from "@mui/icons-material/Search";
-import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined";
+import RadioButtonUncheckedOutlinedIcon from "@mui/icons-material/RadioButtonUncheckedOutlined";
 import { useState } from "react";
 
 const QuizList = () => {
-    const [quizzes,setQuizzes] = useState([])
-    const [errorMessage,setErrorMessage] = useState('')
-//   const quizzes = [
-//     { id: 1, name: "Untitled Quiz", modifiedDate: "10/30/2023" },
-//     { id: 1, name: "Untitled Quiz", modifiedDate: "10/30/2023" },
-//     { id: 1, name: "Untitled Quiz", modifiedDate: "10/30/2023" },
-//   ];
+  const [quizzes, setQuizzes] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [hover, setHover] = useState(false);
+  //   const quizzes = [
+  //     { id: 1, name: "Untitled Quiz", modifiedDate: "10/30/2023" },
+  //     { id: 1, name: "Untitled Quiz", modifiedDate: "10/30/2023" },
+  //     { id: 1, name: "Untitled Quiz", modifiedDate: "10/30/2023" },
+  //   ];
 
-  const getAllQuizes=async()=>{
+  const getAllQuizes = async () => {
     try {
-        const response = await fetch(`http://localhost:8080/Questions`)
-        const data = await response.json()
-        setQuizzes(data)
-        
+      const response = await fetch(`http://localhost:8080/Questions`);
+      const data = await response.json();
+      setQuizzes(data);
     } catch (error) {
-        setErrorMessage(error.message)
+      setErrorMessage(error.message);
     }
-  }
- 
-  useEffect(()=>{
-    getAllQuizes()
-  },[])
+  };
+
+  useEffect(() => {
+    getAllQuizes();
+  }, []);
 
   return (
     <Container sx={{ p: 3 }}>
       <Box sx={{ display: "flex", alignItems: "center", mb: 3 }}>
-        <Typography variant="h6" sx={{ flexGrow: 1 ,textAlign:'left'}}>
+        <Typography variant="h6" sx={{ flexGrow: 1, textAlign: "left" }}>
           Quizzes
         </Typography>
         <IconButton>
@@ -44,11 +52,32 @@ const QuizList = () => {
       </Box>
 
       {quizzes.map((quiz) => (
-       
-        <Card sx={{ mb: 2,textAlign:'left' }} key={quiz.id} >
-          <CardContent>
-            <InsertDriveFileIcon />
-            <Typography variant="body1">{quiz.questionSetTitle}</Typography>
+        <Card sx={{ mb: 2, textAlign: "left" }} key={quiz.id}>
+          <CardContent
+            sx={{ display: "flex", alignItems: "center" }}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+          >
+            {hover ? (
+              <RadioButtonUncheckedOutlinedIcon size="medium" sx={{ mr: 2 }} />
+            ) : (
+              <InsertDriveFileOutlinedIcon fontSize="medium" sx={{ mr: 2 }} />
+            )}
+            <Typography
+              display={"inline-block"}
+              onClick={()=>console.log(quiz.id)}
+              sx={{
+                fontWeight: "bolder",
+                "&:hover": {
+                  color: "#0075A3",
+                  cursor: "pointer",
+                  // fontWeight: "bolder",
+                },
+              }}
+              variant="body1"
+            >
+              {quiz.questionSetTitle}
+            </Typography>
           </CardContent>
         </Card>
       ))}
