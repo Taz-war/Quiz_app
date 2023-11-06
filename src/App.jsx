1caf13b22632c15583fb085f4b1c58a79df27138
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2023-11-06 10:47:00 +0600

    uff

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       {/* <CreateQuiz /> */}
-      {/* <QuizList /> */}
-      <DataTable />
+      <QuizList />
+      {/* <DataTable /> */}
     </div>
   );
 }
--- src/Pages/test.jsx
+++ src/Pages/test.jsx
@@ -1,67 +1,108 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Paper,
-    Radio,
-    ButtonGroup,
-    Button,
-    InputBase,
-} from '@mui/material';
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Radio,
+  ButtonGroup,
+  Button,
+  InputBase,
+} from "@mui/material";
 
 function DataTable() {
-    return (
-        <div style={{ margin: '20px' }}>
-            <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <ButtonGroup variant="contained" size="small">
-                        <Button>Quizzes</Button>
-                        <Button>Deleted</Button>
-                    </ButtonGroup>
-                </div>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <InputBase
-                        placeholder="Search…"
-                        inputProps={{ 'aria-label': 'search' }}
-                        style={{ paddingLeft: '10px', border: '1px solid #ccc', borderRadius: '4px' }}
-                    />
-                    <Button variant="contained" color="primary" size="small" style={{ marginLeft: '10px' }}>
-                        New Folder
-                    </Button>
-                    <Button variant="contained" color="primary" size="small" style={{ marginLeft: '10px' }}>
-                        Add Quiz
-                    </Button>
-                </div>
-            </div>
+  const [quizzes, setQuizzes] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [hover, setHover] = useState(false);
+//   const quizzes = [
+//     { name: "World Facts Quiz", date: "11/2/2023" },
+//     { name: "Untitled Quiz", date: "11/2/2023" },
+//     { name: "Untitled Quiz", date: "10/18/2023" },
+//   ];
+  const getAllQuizes = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/Questions`);
+      const data = await response.json();
+      setQuizzes(data);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
 
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell></TableCell>
-                            <TableCell>NAME</TableCell>
-                            <TableCell>MODIFIED</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {[{ name: 'World Facts Quiz', date: '11/2/2023' }, { name: 'Untitled Quiz', date: '11/2/2023' }, { name: 'Untitled Quiz', date: '10/18/2023' }].map((row) => (
-                            <TableRow key={row.name}>
-                                <TableCell>
-                                    <Radio color="primary" />
-                                </TableCell>
-                                <TableCell>{row.name}</TableCell>
-                                <TableCell>{row.date}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+  useEffect(() => {
+    getAllQuizes();
+  }, []);
+  return (
+    <div style={{ margin: "20px" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "20px",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <ButtonGroup variant="contained" size="small">
+            <Button>Quizzes</Button>
+            <Button>Deleted</Button>
+          </ButtonGroup>
         </div>
-    );
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <InputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+            style={{
+              paddingLeft: "10px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+            }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            style={{ marginLeft: "10px" }}
+          >
+            New Folder
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            style={{ marginLeft: "10px" }}
+          >
+            Add Quiz
+          </Button>
+        </div>
+      </div>
+
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>NAME</TableCell>
+              <TableCell>MODIFIED</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {quizzes.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>
+                  <Radio color="primary" />
+                </TableCell>
+                <TableCell>{row.questionSetTitle}</TableCell>
+                <TableCell>{row.date}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
 
 export default DataTable;
