82e88f2a0c85b94e3c19158cd8ccd91e22081c2b
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2023-12-11 13:48:12 +0600

    socket connection

--- src/Pages/student_portal/Quiz.jsx
+++ src/Pages/student_portal/Quiz.jsx
@@ -15,7 +15,7 @@ import QuestionTypeMultipleChoice from "../../Student_Components/QuestionTypeMul
 import QuestionTypeShortQuestion from "../../Student_Components/QuestionTypeShortQuestion";
 import QuestionTypeTrueFalse from "../../Student_Components/QuestionTypeTrueFalse";
 import { useLocation } from "react-router-dom";
-
+import io from 'socket.io-client';
 
 
 const QuestionRenderer = ({ question, answer, onChange }) => {
@@ -56,10 +56,27 @@ const Quiz = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState({});
   const currentQuestion = quizData.questions[currentQuestionIndex];
+  const socket = io('http://localhost:5000');
+
+  
 
   const handleNext = () => {
     if (currentQuestionIndex < quizData.questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
+      socket.on('connect', () => {
+        console.log('Connected to server');
+      });
+      socket.emit('joinRoom', 'C7h9EM');
+      socket.emit('sendMessage', { room:'C7h9EM', message:'message' });
+      socket.on('message', (message) => {
+        console.log('fahimTazwer',message);
+      });
+      // socket.on('connectQuiz', () => {
+      //   socket.emit('connectQuiz',{
+      //     text:'"hello im fahim"'
+      //   })
+      //   console.log('Connected to server');
+      // });
     } else {
       // Handle quiz completion
       console.log("Quiz Completed", answers);
