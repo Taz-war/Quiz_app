1e864d1f01dd74b888fd31da4e93710c058dc7a4
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2023-12-21 14:01:56 +0600

    lots of thing happning

--- src/Components/Show_report/ShowReport.jsx
+++ src/Components/Show_report/ShowReport.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ShowReport = () => {
     const location = useLocation();
     const id = location.state?.id;
+    const [errorMessage, setErrorMessage] = useState("");
+    const [report,setReport] =useState([])
+
+    useEffect(async()=>{
+        try {
+            const response = await fetch(`http://localhost:5000/publishedQuestions/${id}`);
+            const data = await response.json();
+            setReport(data);
+          } catch (error) {
+            setErrorMessage(error.message);
+          }
+    },[])
+
     return (
         <>
             <div>
