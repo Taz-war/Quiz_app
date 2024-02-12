6d0e9bb26c144fdc78e47a62d3ad60b6c5d6214e
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2024-02-12 13:37:51 +0600

    okay

--- src/Pages/student_portal/Quiz.jsx
+++ src/Pages/student_portal/Quiz.jsx
@@ -1,53 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Button,
-  TextField,
-  FormControl,
-  FormControlLabel,
-  RadioGroup,
-  Radio,
   Typography,
-  Paper,
-  FormLabel,
   Container,
   Collapse,
   Grid,
 } from "@mui/material";
+import { useLocation, useNavigate } from "react-router-dom";
+import io from "socket.io-client";
+import { Alert, Spin } from "antd";
+import { url } from "../../api";
+
 import QuestionTypeMultipleChoice from "../../Student_Components/QuestionTypeMultipleChoice";
 import QuestionTypeShortQuestion from "../../Student_Components/QuestionTypeShortQuestion";
 import QuestionTypeTrueFalse from "../../Student_Components/QuestionTypeTrueFalse";
-import { useLocation, useNavigate } from "react-router-dom";
-import io from "socket.io-client";
-import { useEffect } from "react";
-import { Alert, Spin } from "antd";
-import { url } from "../../api";
 
 const QuestionRenderer = ({ question, answer, onChange }) => {
   switch (question.QuestionType) {
     case "multipleChoice":
-      return (
-        <QuestionTypeMultipleChoice
-          question={question}
-          answer={answer}
-          onChange={onChange}
-        />
-      );
+      return <QuestionTypeMultipleChoice question={question} answer={answer} onChange={onChange} />;
     case "shortQuestion":
-      return (
-        <QuestionTypeShortQuestion
-          question={question}
-          answer={answer}
-          onChange={onChange}
-        />
-      );
+      return <QuestionTypeShortQuestion question={question} answer={answer} onChange={onChange} />;
     case "trueFalse":
-      return (
-        <QuestionTypeTrueFalse
-          question={question}
-          answer={answer}
-          onChange={onChange}
-        />
-      );
+      return <QuestionTypeTrueFalse question={question} answer={answer} onChange={onChange} />;
     default:
       return null;
   }
@@ -55,72 +30,58 @@ const QuestionRenderer = ({ question, answer, onChange }) => {
 
 const Quiz = () => {
   const location = useLocation();
-  const quizData = location.state?.data;
-  const studenData = location.state?.studentData;
-  const id = location.state?.id;
-  const roomName = location.state?.roomName;
+  const navigate = useNavigate();
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState({});
   const [examStarted, setExamStarted] = useState(false);
-  const [studentInfo, setStudentInfo] = useState({ ...studenData });
-  const currentQuestion = quizData.questions[currentQuestionIndex];
+  const [quizData, setQuizData] = useState({ questions: [] }); // Initialize with empty questions array
+  const studenData = location.state?.studentData;
+  const roomName = location.state?.roomName;
   const socket = io(`${url}`);
-  const navigate = useNavigate();
 
   useEffect(() => {
     socket.on("connect", () => {
       console.log("Connected to server");
     });
-    socket.emit(
-      "joinRoom",
-      roomName,
-      studenData,
-      quizData.questions.length,
-      currentQuestionIndex
-    );
+
+
+    socket.emit("joinRoom", roomName, studenData, quizData.questions.length, currentQuestionIndex);
 
     socket.on("examStarted", (data) => {
-      console.log("examStarted", data.examStarted);
       setExamStarted(data.examStarted);
+      if (data.examStarted) {
+        // Fetch quiz data when exam starts
+        fetchQuizData();
+      }
     });
 
-    socket.on("message", (message) => {
-      console.log("fahimTazwer", message);
-    });
-    // socket.emit("sendMessage", { room: "C7h9EM", message: "message" });
-  }, [socket, currentQuestionIndex]);
+    // return () => {
+    //   socket.off("connect");
+    //   socket.off("examStarted");
+    // };
+  }, [socket, currentQuestionIndex, examStarted]);
+
+  const fetchQuizData = async () => {
+    const id = location.state?.id; // Assuming 'id' is the quiz identifier
+    try {
+      const response = await fetch(`${url}/EditQuiz/${id}`);
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+      const data = await response.json();
+      setQuizData(data); // Assuming the fetched data is the quiz data
+    } catch (error) {
+      console.error("Error fetching quiz data:", error);
+    }
+  };
 
   const handleNext = async () => {
     if (currentQuestionIndex < quizData.questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else if (currentQuestionIndex === quizData.questions.length - 1) {
-      // Handle quiz completion
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-      setExamStarted(false);
-      console.log("Quiz Completed", answers);
-      const updatedStudentInfo = { ...studentInfo, answer: answers };
-      setStudentInfo(updatedStudentInfo);
-      // setStudentInfo({ ...studentInfo, answer: answers });
-
-      try {
-        const response = await fetch(`${url}/student/loginInfo/${id}`, {
-          method: "PUT",
-          body: JSON.stringify(updatedStudentInfo),
-          headers: { "Content-type": "application/json" },
-        });
-
-        if (!response.ok)
-          throw new Error(`HTTP error! status: ${response.status}`);
-        const data = await response.json();
-        console.log(data);
-        navigate("/");
-      } catch (error) {
-        console.error("Error posting question:", error);
-      }
+      // Handle quiz completion logic here
+      navigate("/"); // Navigate to home or results page
     }
   };
 
-  ///handle back button///
   const handleBack = () => {
     if (currentQuestionIndex > 0) {
       setCurrentQuestionIndex(currentQuestionIndex - 1);
@@ -128,72 +89,36 @@ const Quiz = () => {
   };
 
   const handleChange = (event) => {
-    if (currentQuestion && typeof currentQuestion.id !== "undefined") {
-      setAnswers({ ...answers, [currentQuestion.id]: event.target.value });
+    const questionId = quizData.questions[currentQuestionIndex]?.id;
+    if (questionId) {
+      setAnswers({ ...answers, [questionId]: event.target.value });
     }
-    // setAnswers({ ...answers, [currentQuestion.id]: event.target.value });
   };
 
-  ///get quizzes from teacher///
-  useEffect(()=>{
-    fetch(`${url}/EditQuiz/${id}`)
-        .then(res => res.json())
-        .then(data => {
-          
-        })
-  },[examStarted])
-
   const isLastQuestion = currentQuestionIndex === quizData.questions.length - 1;
+  const currentQuestion = quizData.questions[currentQuestionIndex];
 
   return (
     <>
       <Collapse in={examStarted}>
         <div style={{ padding: "20px" }}>
           <Typography variant="h5" style={{ marginBottom: "20px" }}>
-            {`${
-              currentQuestionIndex + 1 < quizData.questions.length
-                ? currentQuestionIndex + 1
-                : quizData.questions.length
-            } of ${quizData?.questions.length || 0}`}
+            {`${currentQuestionIndex + 1} of ${quizData.questions.length}`}
           </Typography>
-          <Container
-            sx={{
-              bgcolor: "#DFEAF3",
-              p: 4,
-              textAlign: "left",
-              justifyContent: "space-between",
-            }}
-            maxWidth="sm"
-          >
+          <Container sx={{ bgcolor: "#DFEAF3", p: 4, textAlign: "left", justifyContent: "space-between" }} maxWidth="sm">
             {currentQuestion && (
-              <QuestionRenderer
-                question={currentQuestion}
-                answer={answers[currentQuestion.id]}
-                onChange={handleChange}
-              />
+              <QuestionRenderer question={currentQuestion} answer={answers[currentQuestion.id]} onChange={handleChange} />
             )}
-            <Grid container columns={12}>
-              <Grid item xs={6} pl={1}>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
                 {currentQuestionIndex > 0 && (
-                  <Button
-                    variant="outlined"
-                    color="error"
-                    onClick={handleBack}
-                    style={{ marginRight: "10px", marginTop: "20px" }}
-                  >
+                  <Button variant="outlined" color="error" onClick={handleBack}>
                     Back
                   </Button>
                 )}
               </Grid>
-              <Grid item xs={6} textAlign={'right'} pr={1}>
-                {/* Modify button text based on question position */}
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleNext}
-                  sx={{ marginTop: "20px", alignSelf: "end" }}
-                  disabled={!currentQuestion} // Disable the button if there's no current question
-                >
+              <Grid item xs={6} style={{ textAlign: "right" }}>
+                <Button variant="contained" color="primary" onClick={handleNext} disabled={!currentQuestion}>
                   {isLastQuestion ? "Submit Quiz" : "Next"}
                 </Button>
               </Grid>
@@ -201,16 +126,13 @@ const Quiz = () => {
           </Container>
         </div>
       </Collapse>
-      <Collapse in={examStarted === false}>
-        <Container
-          sx={{ bgcolor: "#DFEAF3", p: 4, textAlign: "left", mt: 4 }}
-          maxWidth="sm"
-        >
+      <Collapse in={!examStarted}>
+        <Container sx={{ bgcolor: "#DFEAF3", p: 4, textAlign: "left", mt: 4 }} maxWidth="sm">
           <Spin tip="Loading..." size="large">
             <Alert
               type="info"
               message="Quiz hasn't started yet"
-              description="When the quiz starts you'll automatically able to access the quiz"
+              description="When the quiz starts you'll automatically be able to access the quiz."
             />
           </Spin>
         </Container>
--- src/Pages/test.jsx
+++ src/Pages/test.jsx
@@ -1,186 +1,145 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { Radio } from '@mui/material';
-import EnhancedTableHead from '../Components/QuizList_Components/EnhancedTableHead';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Typography,
+  Container,
+  Collapse,
+  Grid,
+} from "@mui/material";
+import { useLocation, useNavigate } from "react-router-dom";
+import io from "socket.io-client";
+import { Alert, Spin } from "antd";
+import { url } from "../../api";
 
+import QuestionTypeMultipleChoice from "../../Student_Components/QuestionTypeMultipleChoice";
+import QuestionTypeShortQuestion from "../../Student_Components/QuestionTypeShortQuestion";
+import QuestionTypeTrueFalse from "../../Student_Components/QuestionTypeTrueFalse";
 
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-// with exampleArray.slice().sort(exampleComparator)
+const QuestionRenderer = ({ question, answer, onChange }) => {
+  switch (question.QuestionType) {
+    case "multipleChoice":
+      return <QuestionTypeMultipleChoice question={question} answer={answer} onChange={onChange} />;
+    case "shortQuestion":
+      return <QuestionTypeShortQuestion question={question} answer={answer} onChange={onChange} />;
+    case "trueFalse":
+      return <QuestionTypeTrueFalse question={question} answer={answer} onChange={onChange} />;
+    default:
+      return null;
+  }
+};
 
+const Quiz = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState({});
+  const [examStarted, setExamStarted] = useState(false);
+  const [quizData, setQuizData] = useState({ questions: [] }); // Initialize with empty questions array
 
-
-export default function EnhancedTable() {
-  const [selected, setSelected] = React.useState([]);
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelected = quizzes.map((n) => n.id);
-      setSelected(newSelected);
-      return;
-    }else{
-      setSelected([]);
-    }
-  };
-
-  const handleClick = (event, id) => {
-    const selectedIndex = selected.indexOf(id);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, id);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
-    setSelected(newSelected);
-  };
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  ////fetch api call////
-  const [quizzes, setQuizzes] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [hover, setHover] = useState(false);
-
-  const getAllQuizes = async () => {
-    try {
-      const response = await fetch(`http://localhost:8080/Questions`);
-      const data = await response.json();
-      setQuizzes(data);
-    } catch (error) {
-      setErrorMessage(error.message);
-    }
-  };
+  const socket = io(`${url}`);
 
   useEffect(() => {
-    getAllQuizes();
-  }, []);
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
 
-  const isSelected = (id) => selected.indexOf(id) !== -1;
+    const studenData = location.state?.studentData;
+    const roomName = location.state?.roomName;
 
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - quizzes.length) : 0;
+    socket.emit("joinRoom", roomName, studenData, quizData.questions.length, currentQuestionIndex);
 
+    socket.on("examStarted", (data) => {
+      setExamStarted(data.examStarted);
+      if (data.examStarted) {
+        // Fetch quiz data when exam starts
+        fetchQuizData();
+      }
+    });
 
+    return () => {
+      socket.off("connect");
+      socket.off("examStarted");
+    };
+  }, [socket, currentQuestionIndex, examStarted]);
+
+  const fetchQuizData = async () => {
+    const id = location.state?.id; // Assuming 'id' is the quiz identifier
+    try {
+      const response = await fetch(`${url}/EditQuiz/${id}`);
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+      const data = await response.json();
+      setQuizData(data); // Assuming the fetched data is the quiz data
+    } catch (error) {
+      console.error("Error fetching quiz data:", error);
+    }
+  };
+
+  const handleNext = async () => {
+    if (currentQuestionIndex < quizData.questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else if (currentQuestionIndex === quizData.questions.length - 1) {
+      // Handle quiz completion logic here
+      navigate("/"); // Navigate to home or results page
+    }
+  };
+
+  const handleBack = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    }
+  };
+
+  const handleChange = (event) => {
+    const questionId = quizData.questions[currentQuestionIndex]?.id;
+    if (questionId) {
+      setAnswers({ ...answers, [questionId]: event.target.value });
+    }
+  };
+
+  const isLastQuestion = currentQuestionIndex === quizData.questions.length - 1;
+  const currentQuestion = quizData.questions[currentQuestionIndex];
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2 }}>
-        {/* <EnhancedTableToolbar numSelected={selected.length} /> */}
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            aria-labelledby="tableTitle"
-            size={ 'medium'}
-          >
-            <EnhancedTableHead
-              numSelected={selected.length}
-              onSelectAllClick={handleSelectAllClick}
-              rowCount={quizzes.length}
+    <>
+      <Collapse in={examStarted}>
+        <div style={{ padding: "20px" }}>
+          <Typography variant="h5" style={{ marginBottom: "20px" }}>
+            {`${currentQuestionIndex + 1} of ${quizData.questions.length}`}
+          </Typography>
+          <Container sx={{ bgcolor: "#DFEAF3", p: 4, textAlign: "left", justifyContent: "space-between" }} maxWidth="sm">
+            {currentQuestion && (
+              <QuestionRenderer question={currentQuestion} answer={answers[currentQuestion.id]} onChange={handleChange} />
+            )}
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                {currentQuestionIndex > 0 && (
+                  <Button variant="outlined" color="error" onClick={handleBack}>
+                    Back
+                  </Button>
+                )}
+              </Grid>
+              <Grid item xs={6} style={{ textAlign: "right" }}>
+                <Button variant="contained" color="primary" onClick={handleNext} disabled={!currentQuestion}>
+                  {isLastQuestion ? "Submit Quiz" : "Next"}
+                </Button>
+              </Grid>
+            </Grid>
+          </Container>
+        </div>
+      </Collapse>
+      <Collapse in={!examStarted}>
+        <Container sx={{ bgcolor: "#DFEAF3", p: 4, textAlign: "left", mt: 4 }} maxWidth="sm">
+          <Spin tip="Loading..." size="large">
+            <Alert
+              type="info"
+              message="Quiz hasn't started yet"
+              description="When the quiz starts you'll automatically be able to access the quiz."
             />
-            <TableBody>
-              {quizzes.map((row, index) => {
-                console.log(row)
-                const isItemSelected = isSelected(row.id);
-                const labelId = `enhanced-table-checkbox-${index}`;
-
-                return (
-                  <TableRow
-                    hover
-                    onClick={(event) => handleClick(event, row.id)}
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
-                    key={row.id}
-                    selected={isItemSelected}
-                    sx={{ cursor: 'pointer' }}
-                  >
-                    <TableCell padding="checkbox">
-                      <Radio
-                        color="primary"
-                        checked={isItemSelected}
-                        inputProps={{
-                          'aria-labelledby': labelId,
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      padding="none"
-                    >
-                      {row.questionSetTitle}
-                    </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    
-                  </TableRow>
-                );
-              })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height:  53 * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={quizzes.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-    </Box>
+          </Spin>
+        </Container>
+      </Collapse>
+    </>
   );
-}
+};
+
+export default Quiz;
